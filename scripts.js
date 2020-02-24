5cd4dfee0ccd077142a4c37deb7f78ca63f85306
Lenito90 <lenito.work@yahoo.com>
2020-02-24 15:01:38 +0200

    Experience panel and hobbies

--- scripts.js
+++ scripts.js
@@ -10,3 +10,16 @@ function togglePanel() {
         currentPanel.classList.add('closed');
     }
 }
+
+document.getElementById('personal-experience-toggle')
+    .addEventListener('click', togglePanel);
+
+
+function togglePanel() {
+    let currentPanel = document.getElementById('personal-experience-panel');
+    if (currentPanel.classList.contains('closed')) {
+        currentPanel.classList.remove('closed');
+    } else {
+        currentPanel.classList.add('closed');
+    }
+}
