e0aa63a47daa7ff3217fd1125ba90216f15d603b
Lenito90 <59535265+Lenito90@users.noreply.github.com>
2020-03-26 20:17:11 +0200

    Fixed toggling of plus/minus buttons

--- scripts.js
+++ scripts.js
@@ -1,8 +1,8 @@
 document.getElementById('design-experience-toggle')
-    .addEventListener('click', togglePanel);
+    .addEventListener('click', toggleDesignPanel);
 
 
-function togglePanel() {
+function toggleDesignPanel() {
     let currentPanel = document.getElementById('design-experience-panel');
     if (currentPanel.classList.contains('closed')) {
         currentPanel.classList.remove('closed');
@@ -12,14 +12,38 @@ function togglePanel() {
 }
 
 document.getElementById('personal-experience-toggle')
-    .addEventListener('click', togglePanel);
+    .addEventListener('click', togglePersonalPanel);
 
 
-function togglePanel() {
+function togglePersonalPanel() {
     let currentPanel = document.getElementById('personal-experience-panel');
     if (currentPanel.classList.contains('closed')) {
         currentPanel.classList.remove('closed');
     } else {
         currentPanel.classList.add('closed');
+    }
+}
+
+document.getElementById('design-elevated-button')
+    .addEventListener('click', toggleDesignButton);
+
+function toggleDesignButton() {
+    let imageInput = document.getElementById('design-elevated-button');
+    if (imageInput.src.indexOf('Assets/plus.svg')!==-1){
+        imageInput.src=imageInput.src.replace('plus.svg','minus.svg');
+    } else{
+        imageInput.src=imageInput.src.replace('minus.svg','plus.svg');
+    }
+}
+
+document.getElementById('personal-elevated-button')
+    .addEventListener('click', togglePersonalButton);
+
+function togglePersonalButton() {
+    let imageInput = document.getElementById('personal-elevated-button');
+    if (imageInput.src.indexOf('Assets/plus.svg')!==-1){
+        imageInput.src=imageInput.src.replace('plus.svg','minus.svg');
+    } else{
+        imageInput.src=imageInput.src.replace('minus.svg','plus.svg');
     }
 }
