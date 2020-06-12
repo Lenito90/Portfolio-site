8ac6669faf3dc4f5c2601970f8aca54dfc06cb12
Lenito90 <59535265+Lenito90@users.noreply.github.com>
2020-06-12 11:30:06 +0300

    Revert "Extended projects, added landing page"
    This reverts commit 800341c8d13e75ecc5ad3d6a9c1ca420b955f8e1.

--- scripts.js
+++ scripts.js
@@ -50,24 +50,3 @@ function togglePersonalButton() {
 }
 
 /* Navigation selection */
-
-/* To top 
-
-var btn = $('#button');
-
-$(window).scroll(function() {
-  if ($(window).scrollTop() > 300) {
-    btn.addClass('show');
-  } else {
-    btn.removeClass('show');
-  }
-});
-
-btn.on('click', function(e) {
-  e.preventDefault();
-  $('html, body').animate({scrollTop:0}, '300');
-});
-
-Code pen: https://codepen.io/matthewcain/pen/ZepbeR
-
-*/
